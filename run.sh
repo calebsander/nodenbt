3729d0a78d67fd22a5527e502f58d08e5f2282be
Caleb Sander <potatoceros@gmail.com>
2014-09-21 16:37:18 -0400

    Added support for nodejs being the node command

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 if ! type "xdg-open" > /dev/null; then
-	open http://localhost:8080;
+	open http://localhost:8080
 else
 	xdg-open http://localhost:8080
 fi
-node nbt.js;
+if type "node" > /dev/null; then
+	node nbt.js
+else
+	nodejs nbt.js
+fi 
