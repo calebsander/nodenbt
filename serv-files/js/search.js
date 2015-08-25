f9ae6bd64bfcb19837554509c59d8dc7012d869f
Caleb Sander <caleb.sander@gmail.com>
2015-08-24 21:10:24 -0400

    Fixed showing elements

--- serv-files/js/search.js
+++ serv-files/js/search.js
@@ -17,15 +17,10 @@ function updateSearchResults() { //display new results
 			path = getPath(parent);
 			breadcrumb = $('<ol>').addClass('breadcrumb').click(function(parent) {
 				return function() { //wrapped so that it will work even when the value of parent is changed
-					$('span.highlight').removeClass('highlight');
-					showElement(parent, true);
-					parent.children('span').addClass('highlight');
-					/*var parentYOffset = parent.offset().top;
-					var parentHeight = parent.height();
-					$('html, body').animate({
-						'scrollTop': parentYOffset - ((innerHeight / 2) - (parentHeight / 2))
-					}, 500);*/
-					parent.get(0).scrollIntoView();
+					$('span.highlight').removeClass('highlight'); //stop highlighting any other spans
+					showElement(parent, true); //open all the parents to that element
+					parent.children('span').addClass('highlight'); //highlight this span
+					parent.get(0).scrollIntoView(); //make sure it is on the page
 				};
 			}(parent));
 			path.forEach(function(segment) { //add each element of the path to the breadcrumb
@@ -36,10 +31,9 @@ function updateSearchResults() { //display new results
 	});
 }
 function showElement(element, skip) { //show all the parent elements to this element (will skip the most-nested one); called recursively
-	if (!element.hasClass('shown')) { //once we get to a shown element, stop showing its parents
-		if (!skip) $.proxy(toggleContainer, element.children('img.type'))(); //call toggleContainer on a child of the li to show this tag
-		showElement(element.parent().parent()); //li -> ul.container -> li
-	}
+	if (!(element.hasClass('shown') || skip)) $.proxy(toggleContainer, element.children('img.type'))(); //call toggleContainer on a child of the li to show this tag if hidden
+	var nextParent = element.parent().parent(); //li -> ul.container -> li
+	if (nextParent.is('li')) showElement(nextParent); //stop once reaching the top of the display chain
 }
 function closeSearch() { //close the search panel and reset the input
 	$('div#search-panel').hide();
